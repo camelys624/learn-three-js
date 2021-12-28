91cd0436ae70d6e841383e43164d5eea1fcc9733
camelys624 <camel_yangz@163.com>
2021-12-28 22:15:06 +0800

    learn threejs init utils part2

--- src/utils/index.js
+++ src/utils/index.js
@@ -378,3 +378,78 @@ export function createGhostTexture() {
   texture.needsUpdate = true;
   return texture;
 }
+
+/**
+ * Add a folder to the gui containing the basic material properties.
+ *
+ * @param gui the gui to add to
+ * @param controls the current controls object
+ * @param material the material to control
+ * @param name optionally the name to assign to the folder
+ */
+export function addBasicMaterialSetting(gui, controls, material, name) {
+  let folderName = (name !== undefined) ? name : 'THREE.Material';
+
+  controls.material = material;
+
+  let folder = gui.addFolder(folderName);
+  folder.add(controls.material, 'id');
+  folder.add(controls.material, 'uuid');
+  folder.add(controls.material, 'name');
+  folder.add(controls.material, 'opacity', 0, 1, 0.01);
+  folder.add(controls.material, 'transparent');
+  folder.add(controls.material, 'overdraw', 0, 1, 0.01);
+  folder.add(controls.material, 'visible');
+  folder.add(controls.material, 'side', {FrontSide: 0, BackSide: 1, BothSides: 2}).onChange(side => {
+    controls.material.side = parseInt(side);
+  });
+
+  folder.add(controls.material, 'colorWrite');
+  folder.add(controls.material, 'flatShading').onChange(shading => {
+    controls.material.flatShading = shading;
+    controls.material.needsUpdate = true;
+  });
+  folder.add(controls.material, 'premultipliedAlpha');
+  folder.add(controls.material, 'dithering');
+  folder.add(controls.material, 'shadowSide', {FrontSide: 0, BackSide: 1, BothSides: 2});
+  folder.add(controls.material, 'vertexColors', {NoColors: THREE.NoColors, FaceColors: THREE.FaceColors, VertexColors: THREE.VertexColors}).onChange(vertexColors => {
+    material.vertexColors = parseInt(vertexColors);
+  });
+  folder.add(controls.material, 'fog');
+
+  return folder;
+}
+
+export function addSpecificMaterialSetting(gui, controls, material, name) {
+  controls.material = material;
+
+  let folderName = (name !== undefined) ? name : 'THREE.' + material.type;
+  let folder = gui.addFolder(folderName);
+
+  switch (material.type) {
+    case 'MeshNormalMaterial':
+      folder.add(controls.material, 'wireframe');
+      return folder;
+    case 'MeshPhongMaterial':
+      controls.specular = material.specular.getStyle();
+      folder.addColor(controls, 'specular').onChange(e => {
+        material.specular.setStyle(e);
+      });
+      folder.add(material, 'shininess', 0, 100, 0.01);
+      return folder;
+    case 'MeshStandardMaterial':
+      controls.color = material.color.getStyle();
+      folder.addColor(controls, 'color').onChange(e => {
+        material.color.setStyle(e);
+      });
+      controls.emissive = material.emissive.getStyle();
+      folder.addColor(controls, 'emissive').onChange( e => {
+        material.emissive.setStyle(e);
+      });
+      folder.add(material, 'metalness', 0, 1, 0.01);
+      folder.add(material, 'roughness', 0, 1, 0.01);
+      folder.add(material, 'wireframe');
+
+      return folder;
+  }
+}
